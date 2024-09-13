ebbadb3e6a954fe70832a4ecb40832ebbe1ed970
David Augustynski <daugustynsk@cps.edu>
2024-09-13 11:09:31 -0500

    completed factorial

--- a1.py
+++ a1.py
@@ -39,7 +39,11 @@ def factorial(n: int) -> int:
     Returns:
         factorial of the passed in number
     """
-    raise NotImplementedError("factorial")
+    result=1
+    for x in range(1,n+1):
+        result *=x
+    print(result)
+    return result
 
 
 T = TypeVar("T")
